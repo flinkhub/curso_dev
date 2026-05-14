6a75bbef35675dae1f5376a66e760201c9f55678
flinkhub <adm.flinkhub@gmail.com>
2026-05-14 22:01:50 +0000

    ajuste no return na Home do next

---
+++ meu-app/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-1 items-center justify-center">
+      Safira, Safirinha, eu te amo muito, muito mais que tudo na minha vidinha. ❤️
+    </div>
+  );
+}
